0ba8f007bf0ef309a9fe05023430674c4444d978
Bruno Delb <brunodelb@gmail.com>
2018-08-21 23:17:47 +0200

    Add Checkstyle

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -2,6 +2,9 @@ package com.leszko.calculator;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Calculator Spring Application.
+ */
 @Service
 public class Calculator {
      int sum(int a, int b) {
--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -3,10 +3,13 @@ package com.leszko.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Spring Application.
+ */
 @SpringBootApplication
 public class CalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(CalculatorApplication.class, args);
+}
 }
